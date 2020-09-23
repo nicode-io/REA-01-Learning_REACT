764a08c9fc9886d7a3a666e2d04806af4c794830
Nicode.IO <nicolas.denoel@gmail.com>
2020-09-23 15:26:24 +0200

    Upload Arnaud check

--- 2-javascript_for_react/memento2.js
+++ 2-javascript_for_react/memento2.js
@@ -5,43 +5,79 @@
 // - Simple promise with Fetch 
 // - Async / Await
 
-// Fetch - XMLHttpRequest mandatory line
-    const { rejects } = require("assert");
-const fetch = require("node-fetch"); // Allow use of FETCH 
+// Fetch mandatory line
+    const fetch = require("node-fetch"); // Allow use of FETCH 
+
+//  Simple promise with Fetch
+    fetch('https://api.randomuser.me/?nat=BE&results=1')
+    .then(res => res.json())
+    .then(json => json.results)
+    .then(console.log)
+    .catch(console.error);
+
+//  Async / Await (Undefined)
+    // const getFakePerson = async () => {
+    //     try {
+    //         let res = await fetch('https://api.randomuser.me/?nat=EU&results=1');
+    //         let {results} = res.json();
+    //         console.log(results);
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // };
+    // getFakePerson();
+
+//  Building Promises (not working)
     const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+//     const getFakeMembers = count => new Promise((resolves, rejects) => {
+//     const api = `https://api.randomuser.me/?nat=US&results=${count}`
+//     const request = new XMLHttpRequest()
+//     request.open('GET', api)
+//     request.onload = () =>
+//          (request.status === 200) ?
+//           resolves(JSON.parse(request.response).results) :
+//           rejects(Error(request.statusText))
+//     request.onerror = (err) => rejects(err)
+//     request.send()
+//   })
 
-// //  Simple promise with Fetch
-//     fetch('https://api.randomuser.me/?nat=BE&results=1')
-//     .then(res => res.json())
-//     .then(json => json.results)
-//     .then(console.log)
-//     .catch(console.error);
+//   getFakeMembers(5).then(
+//     members => console.log(members),
+//     err => console.error(
+//         new Error("cannot load members from randomuser.me"))
+//   )
 
-// //  Async / Await
-//     const getFakePerson = async () => {
-//         try {
-//             let res = await fetch('https://api.randomuser.me/?nat=EU&results=1');
-//             let { results } = res.json();
-//             console.log(results);
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     };
-//     getFakePerson();
+// Classes
+    class Vacation {
+        constructor(destination, length) {
+        this.destination = destination;
+        this.length = length;
+        }
 
-//  Building Promises
-    const getPeople = count =>
-        new Promise((resolves, rejects) => {
-            const api = `https://api.randomuser.me/?nat=US&results=${count}`;
-            const request = new XMLHttpRequest();
-            request.open('GET', api);
-            request.onload = () =>
-                request.status === 200
-                    ? resolves(JSON.parse(request.response).results)
-                    : reject(Error(request.statusText));
-                request.onerror = err => rejects(err);
-                request.send();
-        });
-    getPeople()
+        print() {
+            console.log(`${this.destination} will take ${this.length} days.`);
+        }
+    }
+    const trip = new Vacation('Santiago, Chile', 7);
+    trip.print(); // Result appear upside the fetch in console ;)
+    console.log('');
 
-    
+    class Expedition extends Vacation {
+        constructor(destination, length, gear) {
+            super(destination, length);
+            this.gear = gear;
+        }
+
+        print() {
+            super.print();
+            console.log(`Bring your ${this.gear.join(" and your ")}`);        
+        }
+    }
+    trip_two = new Expedition('Himalaya, India', 20, [
+        'sunglasses',
+        'oxygen',
+        'prayer flags',
+        'drone',
+    ])
+    trip_two.print();
+    console.log('');
